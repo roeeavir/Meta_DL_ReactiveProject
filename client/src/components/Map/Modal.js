07ff0a4e6701af5fd752e956bc68b96fe8c1a244
roeeavir <44678382+roeeavir@users.noreply.github.com>
2022-03-07 18:16:19 +0200

    Can now open a different modal for each user type (guest, registered user, land owner)

--- client/src/components/Map/Modal.js
+++ client/src/components/Map/Modal.js
@@ -16,7 +16,7 @@ function Modal(props) {
     setUserToken(getUserToken());
     user = await dispatch(getUserByToken(userToken))
     console.log("User : ", user)
-    if (user.userName != undefined) {
+    if (user == undefined) {
       setUserLandRelationship("Guest")
     } else if (user.userName == props.landOwner) {
       setUserLandRelationship("Land Owner")
@@ -31,7 +31,11 @@ function Modal(props) {
     props.onCancel();
   }
 
+  if (!userLandRelationship)
+    return null;
+
  // if (user == undefined || user == null || user == 'undefined') { // Guest - Can only view and play the game
+ if (userLandRelationship == "Guest") {
       return (
     <div className='modal'>
       <h3>{userLandRelationship}</h3>
@@ -47,39 +51,39 @@ function Modal(props) {
       </button>
     </div>
     );
-  //} else if (user.userName == props.landOwner) { // Land owner - Controlls the land API
-  //         return (
-  //   <div className='modal'>
-  //     <h3>{userLandRelationship}</h3>
-  //     <br/>
-  //     <p>ID: {props.landId}</p>
-  //     <p>Type: {props.landType}</p>
-  //     <p>Owner: {props.landOwner}</p>
-  //     <p>Price: {props.landPrice}</p>
-  //     <p>For Sale: {props.landForSale}</p>
-  //     <p>Game: {props.landGame}</p>
-  //     <button className='btn btn--alt' onClick={cancelHandler}>
-  //       Cancel
-  //     </button>
-  //   </div>
-  //   );
-  // } else { // Registered owner which does not own the land - Can buy land and play the game
-  //   return (
-  //     <div className='modal'>
-  //     <h3>{userLandRelationship}</h3>
-  //     <br/>
-  //     <p>ID: {props.landId}</p>
-  //     <p>Type: {props.landType}</p>
-  //     <p>Owner: {props.landOwner}</p>
-  //     <p>Price: {props.landPrice}</p>
-  //     <p>For Sale: {props.landForSale}</p>
-  //     <p>Game: {props.landGame}</p>
-  //     <button className='btn btn--alt' onClick={cancelHandler}>
-  //       Cancel
-  //     </button>
-  //   </div>
-  //   );
-  // }
+  } else if (userLandRelationship == "Land Owner") { // Land owner - Controlls the land API
+          return (
+    <div className='modal'>
+      <h3>{userLandRelationship}</h3>
+      <br/>
+      <p>ID: {props.landId}</p>
+      <p>Type: {props.landType}</p>
+      <p>Owner: {props.landOwner}</p>
+      <p>Price: {props.landPrice}</p>
+      <p>For Sale: {props.landForSale}</p>
+      <p>Game: {props.landGame}</p>
+      <button className='btn btn--alt' onClick={cancelHandler}>
+        Cancel
+      </button>
+    </div>
+    );
+  } else { // Registered owner which does not own the land - Can buy land and play the game
+    return (
+      <div className='modal'>
+      <h3>{userLandRelationship}</h3>
+      <br/>
+      <p>ID: {props.landId}</p>
+      <p>Type: {props.landType}</p>
+      <p>Owner: {props.landOwner}</p>
+      <p>Price: {props.landPrice}</p>
+      <p>For Sale: {props.landForSale}</p>
+      <p>Game: {props.landGame}</p>
+      <button className='btn btn--alt' onClick={cancelHandler}>
+        Cancel
+      </button>
+    </div>
+    );
+  }
 
 
   }
